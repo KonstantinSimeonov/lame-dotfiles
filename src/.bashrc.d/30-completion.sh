a20e6e61254b824ebc5a177af8c6dc49f79aff52
Konstantin Simeonov <kon.simeonov@protonmail.com>
2025-10-25 22:35:01 +0300

    feat(bash): pnpm completion

--- src/.bashrc.d/30-completion.sh
+++ src/.bashrc.d/30-completion.sh
@@ -3,3 +3,25 @@ if [ -f /usr/share/bash-completion/bash_completion ]; then
 elif [ -f /etc/bash_completion ]; then
   source /etc/bash_completion
 fi
+
+cached_completion() {
+  local cmd="$1"
+  local cache_cmd="$2"
+
+  if command -v "$cmd" >/dev/null 2>&1; then
+    local cache="$HOME/.bash_completion_cache"
+    mkdir -p "$cache"
+
+    local cache_file="$cache/$cmd-completion.bash"
+
+    echo "$cache_file"
+
+    if [ ! -f "$cache_file" ]; then
+      $cache_cmd > "$cache_file"
+    fi
+
+    source "$cache_file"
+  fi
+}
+
+cached_completion pnpm "pnpm completion bash"
